071c7c0b92373a14c8a487e8230a91efe68b1e98
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-04-20 18:24:01 -0300

    created API_URL with the key of the movie API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import { useEffect } from 'react';
+// 7cb372bc my API key
+
+const API_URL = 'http://www.omdapi.com?apikey=7cb372bc';
+
 
 const App = () => {
+
+    const searchMovies = async (title)=>{
+        const response = await fetch(`${API}`);
+    }
+
+    useEffect(()=>{
+
+    }, []);
+
     return (
         <h1>App</h1>
     );
