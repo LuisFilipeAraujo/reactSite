97107309c55c75f906551345e4011222445a1fe7
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-04-22 14:42:42 -0300

    testing API with a simple search

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import { useEffect } from 'react';
 
 // 7cb372bc my API key
 
-const API_URL = 'http://www.omdapi.com?apikey=7cb372bc';
+const API_URL = 'http://www.omdbapi.com?apikey=7cb372bc';
+
 
 
 const App = () => {
 
     const searchMovies = async (title)=>{
         const response = await fetch(`${API_URL}&s=${title}`); //calling the API
+        const data = await response.json();
+
+        console.log(data.Search);
     }
 
     useEffect(()=>{
-
+        searchMovies('Spiderman');
     }, []);
-
+    
     return (
         <h1>App</h1>
     );
