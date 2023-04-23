0802c586ef668ddbdbfa13274926b670a9ae921e
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-04-23 00:46:25 -0300

    generating an initicial movie poster with Year in it

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=7cb372bc';
 
-
+const movie1 = {
+    "Title": "Spiderman and Grandma",
+    "Year": "2009",
+    "imdbID": "tt1433184",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
+}
 
 const App = () => {
 
@@ -25,11 +31,29 @@ const App = () => {
         <div className="app">
             <h1>MovieLand</h1>
 
+            {/* SearchDIV */}
             <div className="search">
                 <input
                     placeholder="Search for Movies"
                     value="Superman"  
+                    onChange={() => {}}
                 />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+            
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster} alt={movie1.Title}></img>
+                    </div>
+                </div>
             </div>
         </div>
     );
