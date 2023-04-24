d0f6d1a0cd4ae522cbd148450e648e01014c3607
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-04-23 21:41:24 -0300

    finished class container with static method to search specific movie

--- src/App.js
+++ src/App.js
@@ -51,7 +51,12 @@ const App = () => {
                         <p>{movie1.Year}</p>
                     </div>
                     <div>
-                        <img src={movie1.Poster} alt={movie1.Title}></img>
+                        <img src={movie1.Poster !=='N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}></img>
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
                     </div>
                 </div>
             </div>
